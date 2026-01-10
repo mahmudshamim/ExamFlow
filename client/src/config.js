8a1536bceab24106334b5b40736d1f3c05b11ed0
Mahmud <mahmud@Mahmuds-MacBook-Air.local>
2026-01-10 21:54:07 +0600

    Update backend URL and add seeding script

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://micro-task-beta.vercel.app/api';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://exam-flow-server.vercel.app/api';
 
 export default API_URL;
